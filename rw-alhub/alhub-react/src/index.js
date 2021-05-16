6ec98bc0375d2ec0335e19ed47810d2bf656f04d
rwoj <rwoj@poczta.onet.pl>
2021-05-16 14:07:44 +0200

    rw-alhub

---
+++ rw-alhub/alhub-react/src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { addLocaleData } from "react-intl";
+import en from "react-intl/locale-data/en";
+import ru from "react-intl/locale-data/ru";
+import pl from "react-intl/locale-data/pl";
+import { composeWithDevTools } from "redux-devtools-extension";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./rootReducer";
+import { userFetched, fetchCurrentUser } from "./actions/users";
+import { localeSet } from "./actions/locale";
+import setAuthorizationHeader from "./utils/setAuthorizationHeader";
+
+addLocaleData(en);
+addLocaleData(ru);
+addLocaleData(pl);
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+if (localStorage.bookwormJWT) {
+  setAuthorizationHeader(localStorage.bookwormJWT);
+  store.dispatch(fetchCurrentUser());
+} else {
+  store.dispatch(userFetched({}));
+}
+
+if (localStorage.alhubLang) {
+  store.dispatch(localeSet(localStorage.alhubLang));
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <Route component={App} />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+registerServiceWorker();
---
+++ rw-alhub/alhub-react/src/messages.js
@@ -0,0 +1,12 @@
+export default {
+  en: {
+    "nav.dashboard": "Dashboard"
+  },
+  ru: {
+    "nav.dashboard": "Панель Управления"
+  },
+  pl: {
+    "nav.dashboard": "Mój Panel",
+    "nav.characters": "Moje postaci"
+  }
+};
